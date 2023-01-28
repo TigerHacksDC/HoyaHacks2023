dda04a08d436bacdcb0522b78b21e064754f9fea
Wesley Hoy <113642322+Whoy51@users.noreply.github.com>
2023-01-28 18:19:28 -0500

    Update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from task import Task, tasks
+from department import Department, departments
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -8,10 +9,12 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'aRandomString'
 
-task1 = Task('Task 1', 'This is task 1', 'Task')
 
+class EditTask(FlaskForm):
+    select = SelectField('select', choices=[('Edit Task', 'Edit Task'), ('Delete Task', 'Delete Task')])
+    field = StringField('field', validators=[InputRequired()])
+    submit = SubmitField('Submit')
 
-# Form for adding tasks
 class TaskForm(FlaskForm):
     name = StringField('name', validators=[InputRequired(), Length(min=1, max=16)])
     description = StringField('description', validators=[InputRequired()])
@@ -27,10 +30,20 @@ def index():
 @app.route('/task', methods=['GET', 'POST'])
 def task():
     form = TaskForm()
+    edit = EditTask()
     if form.validate_on_submit():
         Task(name=form.name.data, description=form.description.data, department=form.department.data)
-        return render_template('task.html', form=form, tasks=tasks)
-    return render_template('task.html', form=form, tasks=tasks)
+    return render_template('task.html', form=form, tasks=tasks, edit=edit)
+
+
+# @app.route('/task/<task>')
+# def task(task):
+#     return str(tasks[int(task)].name)
+
+
+@app.route('/departments')
+def departments_list():
+    return render_template('departments.html', departments=departments)
 
 
 if __name__ == '__main__':
--- department.py
+++ department.py
@@ -1,5 +1,8 @@
 departments = []
 
+print(departments.__class__)
+
+
 class Department:
     def __init__(self, name, members):
         self.name = name
